0213fe9288fffe93a8b4237147996be4675478b3
hanspeter schlapbach <hpsof@users.noreply.github.com>
2024-08-26 20:42:18 +0200

    Create deploy-hugo-metanet.cmd

---
+++ deploy-hugo-metanet.cmd
@@ -0,0 +1,43 @@
+@echo off
+rem --  03.08.2023 hpsof
+rem --  version mit keys
+rem --  idee : hugo-webseite auf metanet deployen fuer shop.creathera.ch
+rem --         vorlauefig shop von hugocodex, d.d. ganzes web von dem...
+
+rem if ""%1"" == """" goto needParam
+setlocal
+set user=sofimchsch
+set host=80.74.150.2
+set port=2121
+set rpfad=/creathera.ch/shop
+set localdeployfolder=public
+set ppfad=g:/tools/putty
+PATH=%ppfad%;%PATH%
+set pubkeyfile=rsa-key-20230802-metanet-thinkpad.ppk
+
+rem if ""%1"" == ""0"" goto deploy
+rem call write-current-date.cmd
+
+:deploy
+echo.  set commit-info git...
+call set_git_commit_info.cmd
+echo.  mit hugo version in local public erstellen :
+if exist %localdeployfolder% rmdir /q /s %localdeployfolder%
+call hugo.cmd --baseURL=https://shop.creathera.ch/ --environment=production
+
+echo.  inhalt deploy-verz. loeschen auf remote host :
+plink.exe -l %user% -i %ppfad%/%pubkeyfile%  -ssh %host%   -P %port%  rm -rf %rpfad%/*.*
+
+echo.  aktuellen build auf remote host kopieren :
+pscp -p  -i %ppfad%/%pubkeyfile% -P %port%  -r %localdeployfolder%/*.*    %user%@%host%:%rpfad%   
+goto finito
+
+:needParam
+echo. bitte parameter eingeben fuer erzeugung datei mit current-date:
+echo.   0 = nicht erzeugen, d.h. hpsof hat datum auf vertretungs-start-ANZEIGE gesetzt
+echo.   1 = erzeugen
+echo.
+
+:finito
+echo. *** finito ***
+echo.
